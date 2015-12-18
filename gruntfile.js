c553f9fbc238cbbc6173754d6efce0a1479ad349
Ivan Dokov <ivangdokov@gmail.com>
2015-12-18 12:47:01 +0200

    Updated css minified and js uglifier

--- gruntfile.js
+++ gruntfile.js
@@ -1,263 +1,261 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-	var cfgCache = {};
+    function Config() {
 
-	function Config() {
+        var cfg, config = {};
+        var dest, modules, module, scripts, script, allScripts;
 
-		var cfg, config = {};
-		var view, livereloadPort, modules, module, scripts, script, allScripts = [];
+        cfg = grunt.file.readJSON('gruntfile.cfg');
 
-		cfg = grunt.file.readJSON('gruntfile.cfg');
+        /**
+         * Fix paths to JS modules and scripts
+         */
+        for (dest in cfg.dest) {
+            if (!cfg.dest.hasOwnProperty(dest)) continue;
 
-		/**
-		* Fix paths to JS modules and scripts
-		*/
-		for ( view in cfg.views ) {
-			if ( ! cfg.views.hasOwnProperty(view) ) continue;
+            /**
+             * Fix modules paths
+             */
+            modules = cfg.dest[dest].js.modules || [];
+            for (module in modules) {
+                if (!modules.hasOwnProperty(module)) continue;
+                modules[module] = 'src/modules/js/' + modules[module] + '.js';
+            }
+            cfg.dest[dest].js.modules = modules;
 
-			/**
-			* Fix modules paths
-			*/
-			modules = cfg.views[view].js.modules || [];
-			for ( module in modules ) {
-				if ( ! modules.hasOwnProperty(module) ) continue;
-				modules[module] = 'src/modules/js/' + modules[module] + '.js';
-			}
-			cfg.views[view].js.modules = modules;
-
-			/**
-			* Fix scriptts paths
-			*/
-			scripts = cfg.views[view].js.scripts || [];
-			for ( script in scripts ) {
-				if ( ! scripts.hasOwnProperty(script) ) continue;
-				scripts[script] = 'src/' + view + '/js/' + scripts[script] + '.js';
-			}
-			cfg.views[view].js.scripts = scripts;
-		}
-
-		/**
-		* JS Hint
-		*/
-		config.jshint = {
-			options: {
-				bitwise: true,
-				camelcase: true,
-				eqeqeq: true,
-				forin: true,
-				latedef: 'nofunc',
-				newcap: true,
-				noarg: true,
-				noempty: true,
-				nonbsp: true,
-				quotmark: 'single',
-				undef: true,
-				unused: false,
-				strict: true,
-				trailing: true,
-				browser: true,
-			}
-		};
-		for ( view in cfg.views ) {
-			if ( ! cfg.views.hasOwnProperty(view) ) continue;
-
-			if ( cfg.views[view].js.modules.length ) {
-				config.jshint['modules_'+view] = cfg.views[view].js.modules;
-			}
-			if ( cfg.views[view].js.scripts.length ) {
-				config.jshint[view] = cfg.views[view].js.scripts;
-			}
-		}
+            /**
+             * Fix scriptts paths
+             */
+            scripts = cfg.dest[dest].js.scripts || [];
+            for (script in scripts) {
+                if (!scripts.hasOwnProperty(script)) continue;
+                scripts[script] = 'src/' + dest + '/js/' + scripts[script] + '.js';
+            }
+            cfg.dest[dest].js.scripts = scripts;
+        }
 
 
-		/**
-		* Concat
-		*/
-		config.concat = {
-			options: {
-				separator: ';'
-			}
-		};
-		for ( view in cfg.views ) {
-			if ( ! cfg.views.hasOwnProperty(view) ) continue;
+        /**
+         * JS Hint
+         */
+        config.jshint = {
+            options: {
+                bitwise: true,
+                camelcase: true,
+                eqeqeq: true,
+                forin: true,
+                latedef: 'nofunc',
+                newcap: true,
+                noarg: true,
+                noempty: true,
+                nonbsp: true,
+                quotmark: 'single',
+                undef: true,
+                unused: false,
+                strict: true,
+                trailing: true,
+                browser: true,
+            }
+        };
+        for (dest in cfg.dest) {
+            if (!cfg.dest.hasOwnProperty(dest)) continue;
 
-			allScripts = [];
-
-			modules = cfg.views[view].js.modules;
-			for ( module in modules ) {
-				if ( ! modules.hasOwnProperty(module) ) continue;
-				allScripts.push(modules[module]);
-			}
-
-			scripts = cfg.views[view].js.scripts;
-			for ( script in scripts ) {
-				if ( ! scripts.hasOwnProperty(script) ) continue;
-				allScripts.push(scripts[script]);
-			}
-
-			if ( ! allScripts.length ) continue;
-
-			config.concat[view] = {
-				src: allScripts,
-				dest: 'public/assets/'+ view +'/js/scripts.js',
-			};
-		}
-
-
-		/**
-		* Uglify
-		*/
-		config.uglify = {};
-		for ( view in cfg.views ) {
-			if ( ! cfg.views.hasOwnProperty(view) ) continue;
-
-			if ( ! cfg.views[view].js.minify ) continue;
-
-			config.uglify[view] = {
-				src: 'public/assets/'+ view +'/js/scripts.js',
-				dest: 'public/assets/'+ view +'/js/scripts.js',
-			};
-		}
+            if (cfg.dest[dest].js.modules.length) {
+                config.jshint['modules_' + dest] = cfg.dest[dest].js.modules;
+            }
+            if (cfg.dest[dest].js.scripts.length) {
+                config.jshint[dest] = cfg.dest[dest].js.scripts;
+            }
+        }
 
 
-		/**
-		* LESS
-		*/
-		config.less = {};
-		for ( view in cfg.views ) {
-			if ( ! cfg.views.hasOwnProperty(view) ) continue;
 
-			config.less[view] = {files: {}};
-			config.less[view].files['public/assets/'+ view +'/css/style.css'] = 'src/'+ view +'/less/style.less';
-		}
+        /**
+         * Concat
+         */
+        config.concat = {
+            options: {
+                separator: ';'
+            }
+        };
+        for (dest in cfg.dest) {
+            if (!cfg.dest.hasOwnProperty(dest)) continue;
 
+            allScripts = [];
 
-		/**
-		* PostCSS
-		*/
-		config.postcss = {
-			options: {
-				map: false,
-				processors: [
-					require('autoprefixer')({browsers: ['last 2 versions']})
-				]
-			}
-		};
-		for ( view in cfg.views ) {
-			if ( ! cfg.views.hasOwnProperty(view) ) continue;
+            modules = cfg.dest[dest].js.modules;
+            for (module in modules) {
+                if (!modules.hasOwnProperty(module)) continue;
+                allScripts.push(modules[module]);
+            }
 
-			config.postcss[view] = {
-				src: 'public/assets/'+ view +'/css/style.css',
-				dest: 'public/assets/'+ view +'/css/style.css'
-			};
-		}
+            scripts = cfg.dest[dest].js.scripts;
+            for (script in scripts) {
+                if (!scripts.hasOwnProperty(script)) continue;
+                allScripts.push(scripts[script]);
+            }
 
+            if (!allScripts.length) continue;
 
-		/**
-		* CSS Minifier
-		*/
-		config.cssmin = {files:[]};
-		for ( view in cfg.views ) {
-			if ( ! cfg.views.hasOwnProperty(view) ) continue;
-
-			if ( ! cfg.views[view].less.minify ) continue;
-
-			config.cssmin[view] = {
-				files: [
-					{ src: 'public/assets/'+ view +'/css/style.css', dest: 'public/assets/'+ view +'/css/style.css' }
-				]
-			};
-		}
+            config.concat[dest] = {
+                src: allScripts,
+                dest: 'public/assets/' + dest + '/js/scripts.js',
+            };
+        }
 
 
-		/**
-		* SVG Store
-		*/
-		config.svgstore = {
-			options: {
-				prefix: 'svg-',
-				cleanup: true,
-				svg: {
-					style: 'display:none',
-				},
-			}
-		};
-		for ( view in cfg.views ) {
-			if ( ! cfg.views.hasOwnProperty(view) ) continue;
 
-			config.svgstore[view] = {files: {}};
-			config.svgstore[view].files['public/assets/'+ view +'/svg/shapes.svg'] = ['src/'+ view +'/svg/*.svg'];
-		}
+        /**
+         * Uglify
+         */
+        config.uglify = {};
 
-		/**
-		* Watch
-		*/
-		config.watch = {
-			options: {
-				spawn: false,
-				livereload: cfg.livereloadPort || 35730,
-			}
-		};
+        if (cfg.options.minify_js) {
+            for (dest in cfg.dest) {
+                if (!cfg.dest.hasOwnProperty(dest)) continue;
 
-		for ( view in cfg.views ) {
-			if ( ! cfg.views.hasOwnProperty(view) ) continue;
+                config.uglify[dest] = {
+                    src: 'public/assets/' + dest + '/js/scripts.js',
+                    dest: 'public/assets/' + dest + '/js/scripts.js',
+                };
+            }
+        }
 
-			config.watch[view + '_css'] = {
-				files: ['src/'+ view +'/less/*.less'],
-				//tasks: ['less:'+ view, 'postcss:autoprefixer:'+ view]
-				tasks: ['less:'+ view, 'postcss:'+ view]
-			};
-			if ( cfg.views[view].less.minify )
-				config.watch[view + '_css'].tasks.push('cssmin:'+ view);
 
-			config.watch[view + '_js'] = {
-				files: ['src/'+ view +'/js/**/*.js'],
-				tasks: ['jshint:'+ view, 'concat:'+ view]
-			};
-			if ( cfg.views[view].js.minify )
-				config.watch[view + '_js'].tasks.push('uglify:'+ view);
 
-			config.watch[view + '_svg'] = {
-				files: ['src/'+ view +'/svg/*.svg'],
-				tasks: ['svgstore:'+ view]
-			};
+        /**
+         * LESS
+         */
+        config.less = {};
+        for (dest in cfg.dest) {
+            if (!cfg.dest.hasOwnProperty(dest)) continue;
 
-			config.watch[view + '_views'] = {files: []};
-			for ( var tpl in cfg.views[view].views )
-				config.watch[view + '_views'].files.push(cfg.views[view].views[tpl]);
-		}
+            config.less[dest] = {files: {}};
+            config.less[dest].files['public/assets/' + dest + '/css/style.css'] = 'src/' + dest + '/less/style.less';
+        }
 
-		config.watch.less = {
-			files: ['src/modules/less/*.less'],
-			tasks: ['less', 'cssmin', 'postcss']
-		};
 
-		config.watch.jshint = {
-			files: ['src/modules/js/*.js'],
-			tasks: ['jshint', 'concat', 'uglify'],
-		};
 
-		config.watch.reloadConfig = {
-			files: ['gruntfile.cfg'],
-			tasks: ['reload_config', 'less', 'cssmin', 'postcss', 'jshint', 'concat', 'uglify'],
-		};
+        /**
+         * PostCSS
+         */
+        config.postcss = {
+            options: {
+                map: cfg.options.less_map || false,
+                processors: [
+                    require('autoprefixer')({browsers: ['last 2 versions']})
+                ]
+            }
+        };
 
-		return config;
-	}
+        /**
+         * PostCSS minify
+         */
+        if (cfg.options.minify_css) {
+            config.postcss.options.processors.push(require('cssnano')());
+        }
 
-	grunt.config.init(Config());
+        /**
+         * PostCSS dest
+         */
+        for (dest in cfg.dest) {
+            if (!cfg.dest.hasOwnProperty(dest)) continue;
 
-	require('load-grunt-tasks')(grunt, {pattern: ['grunt-*', '@*/grunt-*']});
+            config.postcss[dest] = {
+                src: 'public/assets/' + dest + '/css/style.css',
+                dest: 'public/assets/' + dest + '/css/style.css'
+            };
+        }
 
-	grunt.registerTask('reloadConfig', "Reload config", function() {
-		grunt.config.data = Config();
-    	console.log("Reloading config");
-	});
 
-	grunt.registerTask('config', "Print config", function() {
-    	console.log(JSON.stringify(Config(), null, 4));
-	});
 
-	grunt.registerTask('default', ['watch']);
+        /**
+         * SVG Store
+         */
+        config.svgstore = {
+            options: {
+                prefix: 'svg-',
+                cleanup: true,
+                svg: {
+                    style: 'display:none'
+                }
+            }
+        };
+        for (dest in cfg.dest) {
+            if (!cfg.dest.hasOwnProperty(dest)) continue;
+
+            config.svgstore[dest] = {files: {}};
+            config.svgstore[dest].files['public/assets/' + dest + '/svg/shapes.svg'] = ['src/' + dest + '/svg/*.svg'];
+        }
+
+
+
+        /**
+         * Watch
+         */
+        config.watch = {
+            options: {
+                spawn: false,
+                livereload: cfg.livereloadPort || 35730
+            }
+        };
+
+        for (dest in cfg.dest) {
+            if (!cfg.dest.hasOwnProperty(dest)) continue;
+
+            config.watch[dest + '_css'] = {
+                files: ['src/' + dest + '/less/*.less'],
+                tasks: ['less:' + dest, 'postcss:' + dest]
+            };
+
+            config.watch[dest + '_js'] = {
+                files: ['src/' + dest + '/js/**/*.js'],
+                tasks: ['jshint:' + dest, 'concat:' + dest, 'uglify:' + dest]
+            };
+
+            config.watch[dest + '_svg'] = {
+                files: ['src/' + dest + '/svg/*.svg'],
+                tasks: ['svgstore:' + dest]
+            };
+
+            config.watch[dest + '_html'] = {files: []};
+            for (var tpl in cfg.dest[dest].html) {
+                if (!cfg.dest[dest].html.hasOwnProperty(tpl)) continue;
+                config.watch[dest + '_html'].files.push(cfg.dest[dest].html[tpl]);
+            }
+        }
+
+        config.watch.css = {
+            files: ['src/modules/less/*.less'],
+            tasks: ['less', 'postcss']
+        };
+
+        config.watch.js = {
+            files: ['src/modules/js/*.js'],
+            tasks: ['jshint', 'concat', 'uglify']
+        };
+
+        config.watch.config = {
+            files: ['gruntfile.cfg'],
+            tasks: ['reload', 'less', 'postcss', 'jshint', 'concat', 'uglify']
+        };
+
+        return config;
+    }
+
+    require('load-grunt-tasks')(grunt, {pattern: ['grunt-*', '@*/grunt-*']});
+
+    grunt.config.init(Config());
+
+    grunt.registerTask('reload', 'Reload config', function () {
+        grunt.config.data = Config();
+        console.log('Reloading config');
+    });
+
+    grunt.registerTask('config', 'Print config', function () {
+        console.log(JSON.stringify(Config(), null, 4));
+    });
+
+    grunt.registerTask('default', ['watch']);
 
 };

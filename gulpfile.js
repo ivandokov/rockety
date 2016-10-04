255344638c2fcb1a9b5230989dbb77e1c58b9bea
Ivan Dokov <ivangdokov@gmail.com>
2016-10-04 17:14:45 +0300

    Added svg task

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,15 @@ var gulp = require('gulp');
 var fs = require('fs');
 var merge = require('merge2');
 var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 var less = require('gulp-less');
 var sass = require('gulp-sass');
 var cleancss = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
+var svgstore = require('gulp-svgstore');
+var svgmin = require('gulp-svgmin');
+var cheerio = require('gulp-cheerio');
 var config = require('js-yaml').safeLoad(fs.readFileSync('rockety.yml', 'utf8'));
 
 gulp.task('config', function () {
@@ -32,13 +36,39 @@ function css(config) {
     }
 
     stream = merge(vendor, css || gulp.src([]));
-    if (config.css.sourcemap) { stream = stream.pipe(sourcemaps.init());}
-    if (config.css.minify) {    stream = stream.pipe(cleancss());}
-                                stream = stream.pipe(concat('style.css'));
-    if (config.css.sourcemap) { stream = stream.pipe(sourcemaps.write());}
+    if (config.css.sourcemap) {
+        stream = stream.pipe(sourcemaps.init());
+    }
+    if (config.css.minify) {
+        stream = stream.pipe(cleancss());
+    }
+    stream = stream.pipe(concat('style.css'));
+    if (config.css.sourcemap) {
+        stream = stream.pipe(sourcemaps.write());
+    }
     return stream.pipe(gulp.dest(config.dest + '/css'));
 }
 
+function svg(config) {
+    return gulp.src(config.source + '/svg/*.svg')
+        .pipe(rename({prefix: 'shape-'}))
+        .pipe(svgmin())
+        .pipe(svgstore())
+        .pipe(cheerio({
+            run: function ($) {
+                $('svg').attr('style', 'display:none');
+            },
+            parserOptions: {xmlMode: true}
+        }))
+        .pipe(rename('shapes.svg'))
+        .pipe(gulp.dest(config.dest + '/svg'));
+}
+
 config.forEach(function (source) {
-    gulp.task('css:' + source.source, function() {return css(source)});
+    gulp.task('css:' + source.source, function () {
+        return css(source)
+    });
+    gulp.task('svg:' + source.source, function () {
+        return svg(source)
+    });
 });

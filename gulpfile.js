7d17c31f7866ce9c7e77f89616a79d7957f45d3f
Ivan Dokov <ivangdokov@gmail.com>
2016-10-04 16:40:07 +0300

    Little refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var fs = require('fs');
+var merge = require('merge2');
+var concat = require('gulp-concat');
 var less = require('gulp-less');
 var sass = require('gulp-sass');
 var cleancss = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
-var merge = require('merge2');
 var config = require('js-yaml').safeLoad(fs.readFileSync('rockety.yml', 'utf8'));
 
 gulp.task('config', function () {
@@ -14,7 +14,7 @@ gulp.task('config', function () {
 });
 
 function css(config) {
-    var stream, vendors = [], vendor, srcLess, srcSass, css;
+    var stream, vendors = [], vendor, css;
 
     (config.css.vendor || []).forEach(function (item) {
         vendors.push('./src/vendor/' + item);
@@ -22,11 +22,9 @@ function css(config) {
     vendor = gulp.src(vendors);
 
     if (config.css.styles) {
-        srcLess = config.css.styles.less ? config.source + '/less/' + config.css.styles.less : [];
-        srcSass = config.css.styles.sass ? config.source + '/sass/' + config.css.styles.sass : [];
         css = merge(
-            gulp.src(srcLess).pipe(less()),
-            gulp.src(srcSass).pipe(sass())
+            gulp.src(config.css.styles.less ? config.source + '/less/' + config.css.styles.less : []).pipe(less()),
+            gulp.src(config.css.styles.sass ? config.source + '/sass/' + config.css.styles.sass : []).pipe(sass())
         ).pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
@@ -38,7 +36,7 @@ function css(config) {
     if (config.css.minify) {    stream = stream.pipe(cleancss());}
                                 stream = stream.pipe(concat('style.css'));
     if (config.css.sourcemap) { stream = stream.pipe(sourcemaps.write());}
-    stream.pipe(gulp.dest(config.dest + '/css'));
+    return stream.pipe(gulp.dest(config.dest + '/css'));
 }
 
 config.forEach(function (source) {
